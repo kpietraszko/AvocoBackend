492c2b9d3ce3cf7552b6d09beb6e1bed72fb5ae3
kpietraszko <krzysztof@pietraszko.cba.pl>
2018-03-22 22:42:22 +0100

    poprawka, ale dalej puste migracje

--- Repository/ApplicationDbContext.cs
+++ Repository/ApplicationDbContext.cs
@@ -17,24 +17,18 @@ namespace Repository
 		//	optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=AvocoBackend;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;");
 		//}
 
-		public DbSet<TestModel> TestModels;
-		//public DbSet<User> Users;
-		//public DbSet<Group> Groups;
-		//public DbSet<GroupJoinedUser> GroupsJoinedUsers;
-		//public DbSet<Message> Messages;
-		//public DbSet<Event> Events;
-		//public DbSet<EventJoinedUser> EventsJoinedUsers;
-		//public DbSet<Friend> Friends;
-		//public DbSet<Post> Posts;
-		//public DbSet<PostComment> PostsComments;
-		//public DbSet<Interest> Interests;
-		//public DbSet<UserInterest> UsersInterests;
-		//public DbSet<GroupInterest> GroupsInterests;
+		public DbSet<User> Users;
+		public DbSet<Group> Groups;
+		public DbSet<GroupJoinedUser> GroupsJoinedUsers;
+		public DbSet<Message> Messages;
+		public DbSet<Event> Events;
+		public DbSet<EventJoinedUser> EventsJoinedUsers;
+		public DbSet<Friend> Friends;
+		public DbSet<Post> Posts;
+		public DbSet<PostComment> PostsComments;
+		public DbSet<Interest> Interests;
+		public DbSet<UserInterest> UsersInterests;
+		public DbSet<GroupInterest> GroupsInterests;
 
-    }
-	public class TestModel
-	{
-		public int TestModelId { get; set; }
-		public string Name { get; set; }
 	}
 }
