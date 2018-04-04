62616ecd431b5327f63dfbf1eeb10e4f1b820c82
kpietraszko <krzysztof@pietraszko.cba.pl>
2018-04-04 19:42:56 +0200

    Rejestracja i logowanie do wspolnego kontrolera i CORS

--- AvocoBackend/Controllers/UserController.cs
+++ AvocoBackend/Controllers/UserController.cs
@@ -4,38 +4,64 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 using AvocoBackend.Data.Models;
+using Repository;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using System.IdentityModel.Tokens.Jwt;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Repository;
 
 namespace AvocoBackend.Api.Controllers
 {
-	[Route("api/[controller]")]
-	public class TokenController : Controller
+	[Route("api/[controller]/[action]")]
+	public class UserController : Controller
 	{
 		private readonly ApplicationDbContext _context;
-		private readonly IConfiguration _config; //dziala z DI
+		private readonly IConfiguration _config;
 		private readonly IPasswordHasher<User> _passwordHasher;
 
-		public TokenController(ApplicationDbContext context, IConfiguration config, IPasswordHasher<User> passwordHasher)
+		public UserController(ApplicationDbContext context, IConfiguration config, IPasswordHasher<User> passwordHasher)
 		{
 			_context = context;
 			_config = config;
 			_passwordHasher = passwordHasher;
 		}
-		
-		[AllowAnonymous]
+
 		[HttpPost]
-		public IActionResult CreateToken([FromForm]LoginModel loginModel) //loginModel przychodzi ok
+		[AllowAnonymous]
+		public async Task<IActionResult> Register([FromBody] RegisterModel userData) //w js body requestu powinno byc new FormData(form), gdzie form to <form/> htmlowy
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+			if(_context.Users.Any(u => u.EmailAddress == userData.EmailAddress))
+			{
+				return StatusCode(422, "Email already used");
+
+			}
+			var hashedPassword = _passwordHasher.HashPassword(null, userData.Password);
+			var newUser = new User{
+				EmailAddress = userData.EmailAddress,
+				PasswordHash = hashedPassword,
+				FirstName = userData.FirstName,
+				LastName = userData.LastName,
+				Region = userData.Region
+			};
+			_context.Users.Add(newUser);
+			await _context.SaveChangesAsync();
+
+			return CreatedAtAction("Register", null);
+		}
+		[HttpPost]
+		public IActionResult Login([FromBody]LoginModel loginModel) //zwraca token
 		{
 			IActionResult response = Unauthorized();
-			var user = Authenticate(loginModel); 
-			if(user != null)
+			var user = Authenticate(loginModel);
+			if (user != null)
 			{
 				var tokenString = BuildToken(user);
 				response = Ok(new { token = tokenString });
@@ -45,21 +71,13 @@ namespace AvocoBackend.Api.Controllers
 		User Authenticate(LoginModel loginModel)
 		{
 			User user = null;
-			//TODO: spr email z bazą, zahashuj haslo i porownac z hashem z bazy
 			var emailMatchingUser = _context.Users.FirstOrDefault(u => u.EmailAddress == loginModel.Email);
-			if(emailMatchingUser != null)
+			if (emailMatchingUser != null)
 			{
 				var hashVerificationResult = _passwordHasher.VerifyHashedPassword(null, emailMatchingUser.PasswordHash, loginModel.Password);
 				if (hashVerificationResult == PasswordVerificationResult.Success)
 					user = emailMatchingUser;
 			}
-
-			//if (loginModel.Email == "user@example.com" &&
-			//	_passwordHasher.VerifyHashedPassword(
-			//		null, _passwordHasher.HashPassword(null, "secretPassword") , loginModel.Password) == PasswordVerificationResult.Success)  //HACK
-			//{
-			//	user = new User(loginModel.Email); //HACK
-			//}
 			return user;
 		}
 		string BuildToken(User user)
@@ -69,7 +87,7 @@ namespace AvocoBackend.Api.Controllers
 			var token = new JwtSecurityToken(
 				issuer: _config["Jwt:Issuer"],
 				audience: _config["Jwt:Issuer"],
-				expires: DateTime.Now.AddMinutes(_config.GetValue<int>("Jwt:TTL", 30)),
+				expires: DateTime.Now.AddMinutes(_config.GetValue<int>("Jwt:TTL", 30)), //czas z configu, jesli w configu brak to 30min
 				signingCredentials: creds
 				);
 			return new JwtSecurityTokenHandler().WriteToken(token);
--- AvocoBackend/Startup.cs
+++ AvocoBackend/Startup.cs
@@ -30,6 +30,7 @@ namespace AvocoBackend
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddCors();
 			services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 			services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 				.AddJwtBearer(options =>
@@ -56,6 +57,8 @@ namespace AvocoBackend
 			{
 				app.UseDeveloperExceptionPage();
 			}
+			app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+
 			app.UseAuthentication();
 
 			app.UseMvc();
