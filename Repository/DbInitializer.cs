440e966f4a4404b6e41486bca23e1d706337a202
Robert <Bob@DESKTOP-T5VIJL4>
2018-05-10 21:25:04 +0200

    Dzieją się niestworzone rzeczy

--- Repository/DbInitializer.cs
+++ Repository/DbInitializer.cs
@@ -67,20 +67,29 @@ namespace AvocoBackend.Repository
 			}
 			if (!context.Events.Any())
 			{
-				context.Events.Add(new Event { GroupId = 1, EventName = "ASG w terenie", EventDateTime = new DateTime(2018, 5, 29, 16, 0, 0), EventLocationLat = 53.777367, EventLocationLng = 20.484959 });
+				context.Events.Add(new Event { GroupId = 1, EventName = "ASG w terenie", EventDescription = "Strzelanie z kulek do ludzi", EventDateTime = new DateTime(2018, 5, 29, 16, 0, 0), EventLocationLat = 53.777367, EventLocationLng = 20.484959 });
+                context.Events.Add(new Event { GroupId = 9, EventName = "Defilada", EventDateTime = new DateTime(2018, 7, 17, 12, 30, 0), EventLocationLat = 53.677367, EventLocationLng = 20.684959 });
+                context.Events.Add(new Event { GroupId = 8, EventName = "Pokaz broni", EventDateTime = new DateTime(2018, 8, 1, 8, 30, 0), EventLocationLat = 53.777367, EventLocationLng = 20.684959 });
                 context.Events.Add(new Event { GroupId = 2, EventName = "Maraton filmowy", EventDateTime = new DateTime(2018, 6, 21, 22, 20, 0), EventLocationLat = 53.477367, EventLocationLng = 20.284959 });
                 context.Events.Add(new Event { GroupId = 3, EventName = "Szkolenie z pythona", EventDateTime = new DateTime(2018, 7, 11, 11, 0, 0), EventLocationLat = 53.977367, EventLocationLng = 20.384959 });
                 context.Events.Add(new Event { GroupId = 4, EventName = "Sesja zdjeciowa", EventDateTime = new DateTime(2018, 6, 1, 8, 0, 0), EventLocationLat = 53.577367, EventLocationLng = 20.584959 });
                 context.Events.Add(new Event { GroupId = 5, EventName = "Bieg wokol jeziora", EventDateTime = new DateTime(2018, 7, 20, 14, 0, 0), EventLocationLat = 52.777367, EventLocationLng = 21.484959 });
                 context.Events.Add(new Event { GroupId = 6, EventName = "Wspolne granie na stadionie", EventDateTime = new DateTime(2018, 7, 11, 20, 0, 0), EventLocationLat = 53.447367, EventLocationLng = 20.224959 });
                 context.Events.Add(new Event { GroupId = 7, EventName = "Prelekcja o ziolach bagiennych", EventDateTime = new DateTime(2018, 6, 20, 13, 0, 0), EventLocationLat = 53.677367, EventLocationLng = 20.784959 });
-                context.Events.Add(new Event { GroupId = 8, EventName = "Defilada", EventDateTime = new DateTime(2018, 7, 17, 12, 30, 0), EventLocationLat = 53.677367, EventLocationLng = 20.684959 });
-                context.Events.Add(new Event { GroupId = 7, EventName = "Pokaz broni", EventDateTime = new DateTime(2018, 8, 1, 8, 30, 0), EventLocationLat = 53.777367, EventLocationLng = 20.684959 });
-			}
+                context.SaveChanges();
+            }
+            /*
             if (!context.Posts.Any()) //tudu
             {
-                context.Posts.Add(new Post { });
-            }
+                context.Posts.Add(new Post {GroupId = 1, UserId = 6, Content = "tesasdasdsadasdasdasd" });
+                context.SaveChanges();
+            }*/
 		}
 	}
 }
+
+// uzywac uzytkowników 1,3,6 
+// 6 - jan
+// 1 - krzysiek
+// 3 - joanna
+// 1 - militaria
